50c2903668c734b1dc41e9b6b036870ea8a72479
viktoriiasokolova17 <viktoriia.sokolova17@gmail.com>
2024-02-21 20:31:56 +0200

    Add swiper to formats-section

---
+++ js/formats-slider.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    }
+  },
+
+  pagination: {
+      el: '.pagination',
+      bulletClass: 'pagination__button',
+      bulletActiveClass: 'pagination__button-active',
+  },
+
+  navigation: {
+    nextEl: '.carousel-button.next',
+    prevEl: '.carousel-button.prev',
+  },
+
+});
